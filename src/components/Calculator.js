aa737da89eaa403289108bf4caf0f3b097f50969
john <kiokoj81@gmail.com>
2022-11-02 14:22:51 +0300

    Removed unneeded classes

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -12,23 +12,23 @@ class Calculator extends React.Component {
       <div className="grid-container">
         <div className="item0">0</div>
         <div className="item1">AC</div>
-        <div className="item2 item1">+/-</div>
-        <div className="item3 item1">%</div>
+        <div className="item1">+/-</div>
+        <div className="item1">%</div>
         <div className="item4"> ÷ </div>
-        <div className="item5 item1">7</div>
-        <div className="item6 item1">8</div>
-        <div className="item7 item1">9</div>
+        <div className="item1">7</div>
+        <div className="item1">8</div>
+        <div className="item1">9</div>
         <div className="item8">x</div>
-        <div className="item9 item1">4</div>
-        <div className="item10 item1">5</div>
-        <div className="item11 item1">6</div>
+        <div className="item1">4</div>
+        <div className="item1">5</div>
+        <div className="item1">6</div>
         <div className="item12">-</div>
-        <div className="item13 item1">1</div>
-        <div className="item14 item1">2</div>
-        <div className="item15 item1">3</div>
+        <div className="item1">1</div>
+        <div className="item1">2</div>
+        <div className="item1">3</div>
         <div className="item16">+</div>
         <div className="item17 item1">0</div>
-        <div className="item18 item1">.</div>
+        <div className="item1">.</div>
         <div className="item19">=</div>
       </div>
     );
