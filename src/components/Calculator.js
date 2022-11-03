d5c964f99acfb32895e2ef39337bed077e048d24
john <kiokoj81@gmail.com>
2022-11-03 13:50:30 +0300

    Refactor class component to use react hook

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,98 +1,83 @@
-import React from 'react';
-import calculate from '../logic/calculate';
-import './App.css';
+import React, { useState } from "react";
+import calculate from "../logic/calculate";
+import "./App.css";
+import { useState } from "react";
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-
-    this.clickFunction = this.clickFunction.bind(this);
+function Calculator() {
+  const [state, setState] = useState({ total: 0, next: null, operation: null });
+  function clickFunction(event) {
+    setState((state) => calculate(state, event.target.innerText));
   }
 
-  clickFunction(event) {
-    this.setState((state) => calculate(state, event.target.innerText));
-  }
+  const { total, next, operation } = state;
 
-  render() {
-    const { total, next, operation } = this.state;
-
-    return (
-      <div className="grid-container">
-        <div className="item0">
-          {total}
-          {operation}
-          {next}
-        </div>
-
-        <button type="button" onClick={this.clickFunction} className="item1">
-          AC
-        </button>
-        <button type="button" onClick={this.clickFunction} className="item1">
-          +/-
-        </button>
-        <button type="button" onClick={this.clickFunction} className="item1">
-          %
-        </button>
-        <button type="button" onClick={this.clickFunction} className="item4">
-          ÷
-        </button>
-        <button type="button" onClick={this.clickFunction} className="item1">
-          7
-        </button>
-        <button type="button" onClick={this.clickFunction} className="item1">
-          8
-        </button>
-        <button type="button" onClick={this.clickFunction} className="item1">
-          9
-        </button>
-        <button type="button" onClick={this.clickFunction} className="item8">
-          x
-        </button>
-        <button type="button" onClick={this.clickFunction} className="item1">
-          4
-        </button>
-        <button type="button" onClick={this.clickFunction} className="item1">
-          5
-        </button>
-        <button type="button" onClick={this.clickFunction} className="item1">
-          6
-        </button>
-        <button type="button" onClick={this.clickFunction} className="item12">
-          -
-        </button>
-        <button type="button" onClick={this.clickFunction} className="item1">
-          1
-        </button>
-        <button type="button" onClick={this.clickFunction} className="item1">
-          2
-        </button>
-        <button type="button" onClick={this.clickFunction} className="item1">
-          3
-        </button>
-        <button type="button" onClick={this.clickFunction} className="item16">
-          +
-        </button>
-        <button
-          type="button"
-          onClick={this.clickFunction}
-          className="item17 item1"
-        >
-          0
-        </button>
-        <button type="button" onClick={this.clickFunction} className="item1">
-          .
-        </button>
-        <button type="button" onClick={this.clickFunction} className="item19">
-          =
-        </button>
+  return (
+    <div className="grid-container">
+      <div className="item0">
+        {total}
+        {operation}
+        {next}
       </div>
-    );
-  }
+
+      <button type="button" onClick={clickFunction} className="item1">
+        AC
+      </button>
+      <button type="button" onClick={clickFunction} className="item1">
+        +/-
+      </button>
+      <button type="button" onClick={clickFunction} className="item1">
+        %
+      </button>
+      <button type="button" onClick={clickFunction} className="item4">
+        ÷
+      </button>
+      <button type="button" onClick={clickFunction} className="item1">
+        7
+      </button>
+      <button type="button" onClick={clickFunction} className="item1">
+        8
+      </button>
+      <button type="button" onClick={clickFunction} className="item1">
+        9
+      </button>
+      <button type="button" onClick={clickFunction} className="item8">
+        x
+      </button>
+      <button type="button" onClick={clickFunction} className="item1">
+        4
+      </button>
+      <button type="button" onClick={clickFunction} className="item1">
+        5
+      </button>
+      <button type="button" onClick={clickFunction} className="item1">
+        6
+      </button>
+      <button type="button" onClick={clickFunction} className="item12">
+        -
+      </button>
+      <button type="button" onClick={clickFunction} className="item1">
+        1
+      </button>
+      <button type="button" onClick={clickFunction} className="item1">
+        2
+      </button>
+      <button type="button" onClick={clickFunction} className="item1">
+        3
+      </button>
+      <button type="button" onClick={clickFunction} className="item16">
+        +
+      </button>
+      <button type="button" onClick={clickFunction} className="item17 item1">
+        0
+      </button>
+      <button type="button" onClick={clickFunction} className="item1">
+        .
+      </button>
+      <button type="button" onClick={clickFunction} className="item19">
+        =
+      </button>
+    </div>
+  );
 }
 
 export default Calculator;
