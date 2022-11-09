a67af6b81934cd3f3e530668974bfa954dc834bb
john <kiokoj81@gmail.com>
2022-11-09 23:16:00 +0300

    Clear linter errors

--- src/pages/__tests__/Home.test.js
+++ src/pages/__tests__/Home.test.js
@@ -1,8 +1,8 @@
-import { render } from "@testing-library/react";
-import React from "react";
-import Home from "../Home";
+import { render } from '@testing-library/react';
+import React from 'react';
+import Home from '../Home';
 
-it("Home renders as expected", () => {
+it('Home renders as expected', () => {
   const Task = render(<Home />);
   expect(Task).toMatchSnapshot();
 });
