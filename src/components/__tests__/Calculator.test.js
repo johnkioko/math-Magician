d5721d5245a6c83a6ac0a929b3029b4fec71ccfa
shella12 <ayeshaarshad4567@gmail.com>
2022-11-10 14:16:29 +0500

    Add test cases for logic

--- src/components/__tests__/Calculator.test.js
+++ src/components/__tests__/Calculator.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 import Calculator from '../Calculator';
+import operate from '../../logic/operate';
+import calculate from '../../logic/calculate';
 
 describe('Test for Calculator', () => {
   test('should render Calculator ', () => {
@@ -8,3 +10,58 @@ describe('Test for Calculator', () => {
     expect(screen).toMatchSnapshot();
   });
 });
+describe('test suite for calculate', () => {
+  const obj = {
+      total: '8',
+      next: '6',
+      operation: '+'
+  }
+  test('when AC button is pressed', () => {
+      const result= calculate(obj,'AC');
+      expect(result.total).toBeNull;
+      expect(result.next).toBeNull;
+      expect(result.operation).toBeNull;
+  })
+  test('when = button is pressed', () => {
+      const result= calculate(obj,'=');
+      expect(result.total).toBe('14');
+      expect(result.next).toBeNull;
+      expect(result.operation).toBeNull;
+  })
+  test('when a number is pressed', () => {
+      const result= calculate(obj,'8');
+      expect(result.total).toBe('8');
+      expect(result.next).toBe('68');
+      expect(result.operation).toBe('+');
+  })
+  test('when an operation is pressed', () => {
+      const result= calculate(obj,'-');
+      expect(result.total).toBe('14');
+      expect(result.next).toBeNull;
+      expect(result.operation).toBe('-');
+  })
+})
+describe('test suite for operate', () => {
+  const numberOne = '4';
+  const numberTwo = '6';
+  test('when + button is pressed', () => {
+      const operation = '+';
+      const result= operate(numberOne,numberTwo,operation);
+      expect(result).toBe('10');
+  })
+  test('when - button is pressed', () => {
+      const operation = '-';
+      const result= operate(numberOne,numberTwo,operation);
+      expect(result).toBe('-2');
+  })
+  test('when x button is pressed', () => {
+      const operation = 'x';
+      const result= operate(numberOne,numberTwo,operation);
+      expect(result).toBe('24');
+  })
+  test('when ÷ button is pressed', () => {
+      const operation = '÷';
+      const result= operate(numberOne,numberTwo,operation);
+      expect(result).toBe('0.66666666666666666667');
+  })
+})
